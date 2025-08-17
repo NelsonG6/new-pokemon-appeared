d70afde4bf395279a1738ef46685bf3e720e25f4
Nelson <nelson@romaine.life>
2025-08-17 16:14:30 -0700

    Tofu init (#4)

---
+++ tofu/main.tf
@@ -0,0 +1,23 @@
+# resource "azurerm_resource_group" "rg" {
+#   name     = "new-pokemon-appears"
+#   location = "WestUS3"
+# }
+
+# resource "azurerm_service_plan" "sp" {
+#   name                = "${azurerm_resource_group.rg.name}-asp"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   os_type             = "Linux"
+#   sku_name            = "P1v2"
+# }
+
+# resource "azurerm_linux_web_app" "example" {
+#   name                = "${azurerm_resource_group.rg.name}-wa"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_service_plan.rg.location
+#   service_plan_id     = azurerm_service_plan.sp.id
+#   application_stack = {
+#     node_version 
+#   }
+#   site_config {}
+# }
