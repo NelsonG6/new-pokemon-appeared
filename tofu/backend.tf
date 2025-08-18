070e49e1b0e3302cc286572968148c2e58b34272
Nelson Romaine <nelson@romaine.life>
2025-08-17 19:26:35 -0700

    more lint addressing

--- tofu/backend.tf
+++ tofu/backend.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = "1.10.5"
   backend "azurerm" {
     use_oidc             = true                                   # Can also be set via `ARM_USE_OIDC` environment variable.
     use_azuread_auth     = true                                   # Can also be set via `ARM_USE_AZUREAD` environment variable.
