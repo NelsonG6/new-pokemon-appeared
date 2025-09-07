c2db252da85eb7074f366980fc9f44d504a590c1
Nelson Romaine <nelson@romaine.life>
2025-09-06 19:56:22 -0700

    attempting to make free tier DB

--- tofu/main.tf
+++ tofu/main.tf
@@ -40,3 +40,41 @@ resource "azurerm_linux_function_app" "fa" {
   service_plan_id            = azurerm_service_plan.sp.id
   site_config {}
 }
+
+resource "azurerm_cosmosdb_account" "db" {
+  name                       = "${azurerm_resource_group.rg.name}-cdba"
+  resource_group_name        = azurerm_resource_group.rg.name
+  location                   = azurerm_resource_group.rg.location
+  offer_type                 = "Standard"
+  kind                       = "MongoDB"
+  automatic_failover_enabled = false
+  free_tier_enabled          = true
+  capabilities {
+    name = "EnableAggregationPipeline"
+  }
+  capabilities {
+    name = "mongoEnableDocLevelTTL"
+  }
+  capabilities {
+    name = "MongoDBv3.4"
+  }
+  capabilities {
+    name = "EnableMongo"
+  }
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+  geo_location {
+    location          = "eastus"
+    failover_priority = 1
+  }
+}
+
+resource "azurerm_cosmosdb_mongo_database" "mongo_db" {
+  name                = "${azurerm_resource_group.rg.name}-db"
+  resource_group_name = azurerm_resource_group.rg.name
+  account_name        = azurerm_cosmosdb_account.db.name
+  throughput          = 400
+}
